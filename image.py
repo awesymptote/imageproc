2a27c165f27fea3cd4d7a268c4762f20097e997f
Kyle <kylestlegerbarter@gmail.com>
2013-09-03 20:28:03 -0700

    added debug output for csv files, haven't tested nonempty files yet

--- image.py
+++ image.py
@@ -4,25 +4,59 @@ import csv
 import os
 
 def main():
+
+	# Set up parser 
 	parser = argparse.ArgumentParser()
-	parser.add_argument("file1",help="first file to compare")
-	parser.add_argument("file2",help="second file to compare")
-	parser.add_argument("-d","--dbg",help="gives detailed csv output",action="store_true")
+	parser.add_argument("file1",help="first file to compare") # <-- required arg
+	parser.add_argument("file2",help="second file to compare") # <-- required arg
+	parser.add_argument("-d","--dbg",help="gives detailed csv output",action="store_true") # <-- optional arg
 	args = parser.parse_args()
 
+	# If files aren't there, abort gracefully with extra kind message
 	if not os.path.exists(args.file1) or not os.path.exists(args.file2):
 		print "One or more of the files does not exist, please check that the file names were entered correctly."
 		exit()
-		
 	
+	# Open csv files
 	file1 = open(args.file1,"rb")
 	file2 = open(args.file2,"rb")
+	
+	read1 = csv.reader(file1, delimiter=' ',quotechar='|')
+	read2 = csv.reader(file2, delimiter=' ',quotechar='|')
 
 
-
-
+	# Print contents of csv file for debug
 	if args.dbg:
-		print "print csv file contents here"
+
+		#this iteration assumes that the first row is a header of some sort, might need to change that 
+		print "csv file 1 contents here" 
+		rownumber = 0
+		for row in read1:
+			if rownumber == 0:
+				header = row
+			else:
+				colnumber = 0
+				for colnumber in row:
+					print '%-8s: %s' % (header[colnumber],col)
+					colnumber += 1
+			rownumber += 1
+
+		print "csv file 2 contents here"
+		rownumber = 0
+		for row in read2:
+			if rownumber == 0:
+				header = row
+			else:
+				colnumber = 0
+				for colnumber in row:
+					print '%-8s: %s' %(header[colnumber],col)
+					colnumber += 1
+			rownumber += 1
+			
+
+
+
+
 
 if __name__ == '__main__':
 	main()
