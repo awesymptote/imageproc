9d3c3e0ac7b778bd5e08afbc9d39cbb0c438c180
Kyle <kylestlegerbarter@gmail.com>
2013-09-08 18:02:35 -0700

    Now creates output file with results.  Added sample data to repo.

--- image.py
+++ image.py
@@ -12,7 +12,7 @@ def main():
 	parser.add_argument("file1",help="first file to compare") # <-- required arg
 	parser.add_argument("file2",help="second file to compare") # <-- required arg
 	parser.add_argument("outfile",help="name of output file") # <-- required arg
-	parser.add_argument("-d","--dbg",help="gives detailed csv output",action="store_true") # <-- optional arg
+	parser.add_argument("-d","--dbg",help="gives normalized delta output",action="store_true") # <-- optional arg
 
 	args = parser.parse_args()
 
@@ -48,17 +48,35 @@ def main():
 			if maxDeltaVal != 0:
 				for col in range(0,30):
 					bigArray[row][col] = float(bigArray[row][col]/maxDeltaVal)
-					print "Normalized diffs: %f" % bigArray[row][col]
+					if args.dbg:
+						print "Normalized deltas: %f" % bigArray[row][col]
 
 		# Random code that just creates white image for now
 
 		img = Image.new('RGB',(400,400),"white")
 		draw = ImageDraw.Draw(img)
-		draw.line((0,0) + img.size, fill=128)
-		draw.line((0,img.size[1],img.size[0],0),fill=128)
+		
+		# drawn delta width will be 10px
+		linewidth = 10
+		ystart = 10
+		xstart = 10
+		for r in range(0,20):
+			xstart = 50
+			for c in range(0,30):
+				draw.line((xstart,ystart,xstart+linewidth,ystart), fill=(red(bigArray[r][c]),0,blue(bigArray[r][c])), width=5)
+				xstart += linewidth
+
+			ystart += 20
 		del draw
 		img.save(args.outfile,"PNG")
 
+# Large deltas = more red
+def red(delta):
+	return int(delta*float(255))
+
+# Small deltas = more blue
+def blue(delta):
+	return int((float(1)-delta)*float(255))
 
 
 if __name__ == '__main__':
